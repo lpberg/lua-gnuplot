221d2ee1e4427d16fdd4ce48ba7826f2b8ea5f3f
Leif Berg <lpberg@iastate.edu>
2013-11-09 11:00:52 -0600

    played with example to explore adding parameters

--- example.lua
+++ example.lua
@@ -5,6 +5,7 @@ AddAppDirectory()
 local gp = runfile[[gnuplot.lua]]
 gp.bin = [["C:\Program Files (x86)\gnuplot\bin\gnuplot.exe"]]
 
+
 local function createFullFilePathForWindows(filename)
 	local filenamefullpath = string.match(getScriptFilename(), "(.-)([^\\]-([^%.]+))$")..filename
 	filenamefullpath = string.gsub(filenamefullpath,"\\","/")
@@ -19,42 +20,45 @@ local g = gp{
     height = 480,
     xlabel = "X axis",
     ylabel = "Y axis",
+	xrange = "[0:10]",
+	yrange = "[0:100]",
     key    = "top left",
     consts = {
         gamma = 2.5
     },
     
     data = {
-        gp.file {   -- plot from a file
-            "myfile",                   -- file path
-            title = "Title 1",          -- optional
-        },
+        -- gp.file {   -- plot from a file
+            -- createFullFilePathForWindows("my_data_file"),                   -- file path
+            -- title = "Title 1",          -- optional
+        -- },
         
-        gp.array {  -- plot from an 'array-like' thing in memory. Could be a
-                    -- numlua matrix, for example.
+        gp.array {  -- plot from an 'array-like' thing in memory. Could be a numlua matrix, for example.
+                   
             {
                 {0,1,2,3,4,5,6,7,8,9},  -- x
-                {3,4,5,6,7,8,9,8,7,6}   -- y
+                {30,40,50,60,70,80,90,80,70,60},   -- y
             },
             
-            title = "Title 2",          -- optional
+            title = "My Bar Graph",          -- optional
             using = {1,2},              -- optional
-            with  = 'linespoints'       -- optional
+            with  = 'lines',       -- optional
+
         },
         
-        gp.func {   -- plot from a Lua function
-            function(x)                 -- function to plot
-                return 3* math.sin(2*x) + 4
-            end,
+        -- gp.func {   -- plot from a Lua function
+            -- function(x)                 -- function to plot
+                -- return 3* math.sin(2*x) + 4
+            -- end,
             
-            range = {-2, 10, 0.01},     -- optional
-            width = 3,                  -- optional
-        },
+            -- range = {-2, 10, 0.01},     -- optional
+            -- width = 3,                  -- optional
+        -- },
         
-        gp.gpfunc { -- plot from a native gnuplot function
-            "gamma*sin(1.8*x) + 3",
-            width = 2,
-        },
+        -- gp.gpfunc { -- plot from a native gnuplot function
+            -- "gamma*sin(1.8*x) + 3",
+            -- width = 2,
+        -- },
     }    
 }:plot(outfile)
 
